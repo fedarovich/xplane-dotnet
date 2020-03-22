defac287e0ed89a737257f1f040645239e634946
Pavel <p.fedarovich@gmail.com>
2020-03-22 22:03:46 +0300

    Added NavAid class.

---
+++ src/XP.SDK/XPLM/NavRef.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XP.SDK.XPLM
+{
+    public partial struct NavRef
+    {
+        public static readonly NavRef NotFound = -1;
+    }
+}
