cd1a81db78e964315999ec9c5bc014b2171ca46b
Pavel <p.fedarovich@gmail.com>
2020-12-24 18:48:55 +0300

    Added Utf8StringLiteralAttribute.

---
+++ src/XP.SDK/Utf8StringLiteralAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace XP.SDK
+{
+    /// <summary>
+    /// Mark partial method returning <see cref="Utf8String"/> with this attribute
+    /// in order to automatically generate the return value from the specified literal.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [Conditional("SAVE_UTF8_LITERAL_IN_METADATA")]
+    public sealed class Utf8StringLiteralAttribute : Attribute
+    {
+        public Utf8StringLiteralAttribute(string literal)
+        {
+            Literal = literal;
+        }
+
+        public string Literal { get; }
+    }
+}
