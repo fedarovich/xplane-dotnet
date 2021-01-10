610410192dc17cee19551ea5d184322a17d8da09
Pavel Fedarovich <p.fedarovich@gmail.com>
2021-01-10 20:22:41 +0300

    Added project XP.SDK.OpenTK.Graphics as a replacement of XP.SDK.OpenToolkit.Graphics.

---
+++ src/XP.SDK.OpenTK.Graphics/OpenGLBindingsContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenTK;
+
+namespace XP.SDK.OpenTK.Graphics
+{
+    /// <summary>
+    /// <see cref="IBindingsContext"/> implementation that can be used to bound OpenGL functions to the OpenToolkit.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class OpenGLBindingsContext : IBindingsContext, IDisposable
+    {
+        private IntPtr _opengl;
+
+        /// <summary>
+        /// Creates a new instance of OpenGLBindingsContext.
+        /// </summary>
+        public OpenGLBindingsContext()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                _opengl = NativeLibrary.Load("opengl32.dll");
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                _opengl = NativeLibrary.Load("libGL.so.1");
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                _opengl = NativeLibrary.Load("/System/Library/Frameworks/OpenGL.framework/OpenGL");
+            else
+                throw new PlatformNotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public IntPtr GetProcAddress(string procName)
+        {
+            NativeLibrary.TryGetExport(_opengl, procName, out var address);
+            return address;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            var opengl = _opengl;
+            if (opengl != IntPtr.Zero)
+            {
+                opengl = Interlocked.CompareExchange(ref _opengl, new IntPtr(0), opengl);
+                if (opengl != IntPtr.Zero)
+                {
+                    NativeLibrary.Free(opengl);
+                }
+            }
+        }
+    }
+}
